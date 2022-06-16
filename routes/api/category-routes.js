6e2d2007077bafb76fdd1801959beaf4db320c11
Lunirs <dhong0925@gmail.com>
2022-06-16 19:47:56 -0400

    Added status 200 response

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -77,6 +77,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: "This category does not exist."});
       return;
     }
+    res.status(200).json(categoryData); 
   } catch (err) {
     res.status(500).json(err);
   }
